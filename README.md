ad1f8137fa58606e7e1076cb9daa7e6e36fe5c35
dozhun <dozhun@outlook.com>
2024-12-02 06:20:28 +0000

    up singularity config

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gitpod
 Learn how to use the Gitpod debugging environment.
+
+Effortlessly Build Functional SIF Images via Cloud Server(Google Compute Engine et al.)
--- singularity/install.sh
+++ singularity/install.sh
@@ -2,21 +2,40 @@
 sudo apt-get update
 # Install debian packages for dependencies
 sudo apt-get install -y \
-   wget \
-   build-essential \
-   libseccomp-dev \
-   libglib2.0-dev \
-   pkg-config \
-   squashfs-tools \
-   cryptsetup \
-   runc
+    autoconf \
+    automake \
+    cryptsetup \
+    fuse2fs \
+    git \
+    fuse \
+    libfuse-dev \
+    libglib2.0-dev \
+    libseccomp-dev \
+    libtool \
+    pkg-config \
+    runc \
+    squashfs-tools \
+    squashfs-tools-ng \
+    uidmap \
+    wget \
+    zlib1g-dev
 
 
-wget https://go.dev/dl/go1.23.3.linux-amd64.tar.gz
+#wget https://go.dev/dl/go1.23.3.linux-amd64.tar.gz
 # Remove any previous Go installation
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.23.3.linux-amd64.tar.gz
 # Add /usr/local/go/bin to the PATH environment variable.
 echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && \
 source ~/.bashrc
+
+
+
+wget https://github.com/sylabs/singularity/releases/download/v4.2.1/singularity-ce-4.2.1.tar.gz
+tar -zxf singularity-ce-4.2.1.tar.gz 
+cd singularity-ce-4.2.1/
+./mconfig
+make -C builddir
+sudo make -C builddir install
+singularity --version 
 
