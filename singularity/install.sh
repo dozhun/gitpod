4745001a79f9547ae48fa2ad00b1c7206478ca86
dozhun <dozhun@outlook.com>
2024-12-23 08:40:20 +0000

    with noninteractive

--- singularity/install.sh
+++ singularity/install.sh
@@ -1,7 +1,8 @@
 # Ensure repositories are up-to-date
-sudo apt-get update
+#sudo netselect-apt
+sudo apt-get update -y 
 # Install debian packages for dependencies
-sudo apt-get install -y \
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
     autoconf \
     automake \
     cryptsetup \
